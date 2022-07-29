b342f1767cec6d1c521ccc78aebc55bb86cc80a8
developermithu <www.mithudas77@gmail.com>
2022-07-30 05:37:20 +0600

    Hero section completed - part 1

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      blackhans: ["Black Han Sans", "sans-serif"],
+      opensans: ["Open Sans", "sans-serif"],
+    },
+
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+
+    extend: {
+      colors: {
+        primary: "#404eed",
+        black: "#23272a",
+      },
+    },
+  },
+  plugins: [],
+};
